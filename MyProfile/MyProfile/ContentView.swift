6fc02bb7bc6db3b3e0b74bd65986f72be8ca9370
azoffoli <azoffoli@MAC-0000779BR.local>
2022-02-19 08:25:47 -0300

    Add profile skeleton

--- MyProfile/MyProfile/ContentView.swift
+++ MyProfile/MyProfile/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("My Profile")
-            .padding()
+        VStack {
+            VStack(spacing: 20.0) {
+                Image("avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90.0, height: 90.0)
+                    .cornerRadius(8.0)
+                    .padding(.top, 20.0)
+                
+                Text("Alessandro Zoffoli")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 174 / 255, green: 204 / 255, blue: 165 / 255))
+                    .font(.system(size: 28))
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .frame(maxHeight: 180)
+            
+            HStack {
+                Text("Software Engineer")
+                    .fontWeight(.light)
+                    .foregroundColor(Color(red: 230 / 255, green: 203 / 255, blue: 168 / 255))
+                    .frame(alignment: .leading)
+                Spacer()
+                Text("Available for work")
+                    .fontWeight(.light)
+                    .foregroundColor(Color(red: 230 / 255, green: 203 / 255, blue: 168 / 255))
+                    .frame(alignment: .leading)
+            }
+            .padding([.leading, .trailing], 20)
+            
+            Spacer()
+        }
+        .background(Color(red: 252 / 255, green: 245 / 255, blue: 220 / 255)) //TODO: Add Gradient
+        
     }
 }
 
