ff4701ee01bd19b550e469d9fae9a158a16afbbc
azoffoli <azoffoli@MAC-0000779BR.local>
2022-02-19 08:14:56 -0300

    Initial commit

---
+++ MyProfile/MyProfile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyProfile
+//
+//  Created by azoffoli on 19/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("My Profile")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
